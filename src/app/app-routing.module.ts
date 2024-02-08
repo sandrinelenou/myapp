f774ccea3b0141ae4d61d9e5f0aabf456abc2143
sandrine Youmbissi <yo.sandra@yahoo.it>
2024-02-08 18:06:30 +0100

    commit2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
-const routes: Routes = [];
+
+  const routes: Routes = [
+    { path: 'home', component: HomeComponent }, // Rotta per la pagina "home"
+    
+  // Rotta per gestire i file nell'asset
+  { path: 'assets/file:///C:/Users/YOUSAN00/Documents/CV_Aubay_Sandrine.pdf', component: HomeComponent }
+  ];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    NgxExtendedPdfViewerModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Il Mio Curriculum';
+  name = 'Youmbissi Ndahe Sandrine Noel';
+  professione = 'Professional Technical Consultant';
+  email = 'yo.sandra@yahoo.it';
+  phone = '3933808003';
+  sitoweb = 'www.tuosito.com';
+  linkedin = 'www.linkedin.com/in/sandrinenoel';
 }
